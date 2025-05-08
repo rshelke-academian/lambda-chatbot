b8b20ad8fdb699438280cc0e62fe6c23f6282feb
rshelke-academian <rshelke@academian.com>
2025-05-08 17:56:03 +0530

    IAM role check

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,6 +11,9 @@ provider "aws" {
   region = var.aws_region
 }
 
+# Get current AWS account ID
+data "aws_caller_identity" "current" {}
+
 # S3 bucket for Lambda deployment
 resource "aws_s3_bucket" "lambda_deployment" {
   bucket = "${var.project_name}-lambda-deployment-${random_string.suffix.result}"
@@ -30,12 +33,12 @@ resource "random_string" "suffix" {
   upper   = false
 }
 
-# Lambda function
+# Lambda function using existing LambdaExecutionRole
 resource "aws_lambda_function" "chatbot" {
   s3_bucket     = aws_s3_bucket.lambda_deployment.id
   s3_key        = "lambda_function.zip"
   function_name = "${var.project_name}-chatbot"
-  role          = aws_iam_role.lambda_role.arn
+  role          = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/LambdaExecutionRole"
   handler       = "lambda_function.lambda_handler"
   runtime       = var.lambda_runtime
   timeout       = var.lambda_timeout
@@ -68,9 +71,6 @@ resource "aws_apigatewayv2_stage" "chatbot_stage" {
 resource "aws_apigatewayv2_integration" "lambda_integration" {
   api_id           = aws_apigatewayv2_api.chatbot_api.id
   integration_type = "AWS_PROXY"
-
-  connection_type    = "INTERNET"
-  description        = "Lambda integration"
   integration_method = "POST"
   integration_uri    = aws_lambda_function.chatbot.invoke_arn
 }
@@ -88,24 +88,6 @@ resource "aws_lambda_permission" "api_gw" {
   function_name = aws_lambda_function.chatbot.function_name
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_apigatewayv2_api.chatbot_api.execution_arn}/*/*"
-}
-
-# IAM role for Lambda
-resource "aws_iam_role" "lambda_role" {
-  name = "${var.project_name}-lambda-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      }
-    ]
-  })
 }
 
 # IAM policies for Lambda
